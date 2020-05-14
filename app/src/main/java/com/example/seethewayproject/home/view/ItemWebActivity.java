d0cb5405d97169f97f52e1c463a36ba52ba07ac1
y1n11n <55310159+y1n11n@users.noreply.github.com>
2020-05-14 08:35:53 +0800

    Update ItemWebActivity.java

--- app/src/main/java/com/example/seethewayproject/home/view/ItemWebActivity.java
+++ app/src/main/java/com/example/seethewayproject/home/view/ItemWebActivity.java
@@ -99,10 +99,4 @@ public class ItemWebActivity extends BaseActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // TODO: add setContentView(...) invocation
-        ButterKnife.bind(this);
-    }
 }
